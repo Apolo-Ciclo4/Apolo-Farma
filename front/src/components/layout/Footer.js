b803723393208225cb47b1aac2312984f360b6b9
cristianneita <70445385+cristianneita@users.noreply.github.com>
2022-10-20 22:23:39 -0500

    Header y carousel
    Barra de navegación con subcategorias, carousel con imagenes

---
+++ front/src/components/layout/Footer.js
@@ -0,0 +1,66 @@
+import React, { Fragment } from 'react';
+
+const Footer = () => {
+    return (
+        <Fragment>
+            <footer class="py-5 my-5 text-white fw-light" style={{background: "#388659"}}>
+                <div class="row text-white">
+                    <div class="col-6 col-md-2 mb-3">
+                        <h5>Apolo Farma</h5>
+                        <ul class="nav flex-column text-white">
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Home</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Features</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Pricing</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">FAQs</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">About</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-6 col-md-2 mb-3">
+                        <h5>Sedes</h5>
+                        <ul class="nav flex-column">
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Bogota</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Cali</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Medellin</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Cartagena</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Tunja</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-6 col-md-2 mb-3">
+                        <h5>Contacto</h5>
+                        <ul class="nav flex-column">
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">apolo@apolosolutions.com</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">pqrapolo@apolosolutions.com</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Call center +57 311 222 3334</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Call center +57 322 322 5334</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Call center +57 312 212 3214</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-5 offset-md-1 mb-3">
+                        <form>
+                            <h5>Suscribite a nuestro newsletter</h5>
+                            <p>Recibiras información sobre promociones y más</p>
+                            <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+                                <label for="newsletter1" class="visually-hidden">Correo Electronico</label>
+                                <input id="newsletter1" type="text" class="form-control" placeholder="Email address"></input>
+                                    <button class="btn btn-outline-light mr-2 ml-2" type="button">Subscribe</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+                    <p>&copy; 2022 Apolo, Inc. All rights reserved.</p>
+                    <ul class="list-unstyled d-flex">
+                        <li class="ms-3"><a class="link-dark" href="#"></a></li>
+                        <li class="ms-3"><a class="link-dark" href="#"></a></li>
+                        <li class="ms-3"><a class="link-dark" href="#"></a></li>
+                    </ul>
+                </div>
+            </footer>
+        </Fragment>
+    )
+}
+export default Footer
--- front/src/components/layout/Header.js
+++ front/src/components/layout/Header.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react';
 const Header = () => {
     return (
         <Fragment>
-            <nav class="navbar navbar-expand-lg navbar-light fixed-top fw-light text-white" style={{background: "#388659"}}>
+            <nav class="navbar navbar-expand-lg navbar-light fixed-top fw-light text-white" style={{ background: "#388659" }}>
                 <div class="container-fluid">
                     <a class="navbar-brand fs-2 text-white" href="#"> <i class="bi bi-heart-pulse-fill text-warning"></i> Apolo Farma</a>
                     <button class="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,14 +12,14 @@ const Header = () => {
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ">
                             <li class="nav-item">
-                                
+
                             </li>
                         </ul>
                         <ul class="navbar-nav mx-auto">
                             <li class="nav-item">
                                 <form class="d-flex align-items-center justify-content-center text-white" role="search">
-                                    <input class="form-control me-2 rounded-pill text-dark" type="search" placeholder="¿Que buscas hoy?" aria-label="Search" style={{width: "300px", height:"50px"}}></input>
-                                    <button class="btn" type="submit" style={{marginLeft: "-10px"}}><i class="bi bi-search text-white"></i></button>
+                                    <input class="form-control me-2 rounded-pill text-dark" type="search" placeholder="¿Que buscas hoy?" aria-label="Search" style={{ width: "300px", height: "50px" }}></input>
+                                    <button class="btn" type="submit" style={{ marginLeft: "-10px" }}><i class="bi bi-search text-white"></i></button>
                                 </form>
                             </li>
                         </ul>
@@ -28,7 +28,7 @@ const Header = () => {
                                 <a href="" className='btn text-white'><i class="bi bi-person fs-2"></i> <h6 className='fw-light'>Iniciar Sesión</h6></a>
                                 <button type="button" class="btn position-relative margin-auto text-white">
                                     <i class="bi bi-cart-plus fs-3"></i>
-                                    <span class="position-absolute top-0 start-75 translate-middle badge rounded-pill bg-danger">
+                                    <span class="position-absolute top-50 start- translate-middle badge rounded-pill bg-danger">
                                         2
                                     </span>
                                     <h6 className='fw-light mt-1 text-white'>Mis compras</h6>
@@ -39,6 +39,92 @@ const Header = () => {
                     </div>
                 </div>
             </nav>
+            <div className='mt-5 pt-5 fw-light'>
+                <nav className='navbar bg-light fw-light '>
+                    <div class="btn-group mt-2">
+                        <button type="button" class="btn btn-light dropdown-toggle fw-light" data-bs-toggle="dropdown" aria-expanded="false">
+                            Medicamentos
+                        </button>
+                        <ul class="dropdown-menu fw-light">
+                            <li><a class="dropdown-item fw-light" href="#">Alivio del dolor</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Sistema Respiratorio</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Metabólicos</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Anticonceptivos</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Salud Digestiva</a></li>
+                        </ul>
+                    </div>
+                    <div class="btn-group mt-2">
+                        <button type="button" class="btn btn-light dropdown-toggle fw-light" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dermocosméticos
+                        </button>
+                        <ul class="dropdown-menu fw-light">
+                            <li><a class="dropdown-item fw-light" href="#">Protección Solar</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Cuidado Facial</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Cuidado de Cabello</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Cuidado Corporal</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Infantil</a></li>
+                        </ul>
+                    </div>
+                    <div class="btn-group mt-2">
+                        <button type="button" class="btn btn-light dropdown-toggle fw-light" data-bs-toggle="dropdown" aria-expanded="false">
+                            Salud Sexual
+                        </button>
+                        <ul class="dropdown-menu fw-light">
+                            <li><a class="dropdown-item fw-light" href="#">Alivio del dolor</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Test de Diagnostico</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Tratamiento Testosterona</a></li>
+                        </ul>
+                    </div>
+                    <div class="btn-group mt-2">
+                        <button type="button" class="btn btn-light dropdown-toggle fw-light" data-bs-toggle="dropdown" aria-expanded="false">
+                            Bebé y maternidad
+                        </button>
+                        <ul class="dropdown-menu fw-light">
+                            <li><a class="dropdown-item fw-light" href="#">Alimentación y lactancia</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Pañales y toallitas humedas</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Salud y Bienestar</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Mamá</a></li>
+                        </ul>
+                    </div>
+                    <div class="btn-group mt-2">
+                        <button type="button" class="btn btn-light dropdown-toggle fw-light" data-bs-toggle="dropdown" aria-expanded="false">
+                            Cuidado Personal
+                        </button>
+                        <ul class="dropdown-menu fw-light">
+                            <li><a class="dropdown-item fw-light" href="#">Cuidado del Cabello</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Cuidado de la piel</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Higiene personal</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Mamá</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Cuidado Masculino</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Cuidado Femenino</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Higiene Oral</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Afeitada y depilación</a></li>
+                        </ul>
+                    </div>
+                    <div class="btn-group mt-2">
+                        <button type="button" class="btn btn-light dropdown-toggle fw-light" data-bs-toggle="dropdown" aria-expanded="false">
+                            Belleza
+                        </button>
+                        <ul class="dropdown-menu fw-light">
+                            <li><a class="dropdown-item fw-light" href="#">Cuidado Corporal</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Cuidado del Cabello</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Accesorios</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Maquillaje</a></li>
+                        </ul>
+                    </div>
+                    <div class="btn-group mt-2">
+                        <button type="button" class="btn btn-light dropdown-toggle dropdown-menu-start fw-light" data-bs-toggle="dropdown" aria-expanded="false">
+                            Bienestar y nutrición
+                        </button>
+                        <ul class="dropdown-menu fw-light">
+                            <li><a class="dropdown-item fw-light" href="#">Alimentos saludables</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Vitaminas y Minerales</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Suplemetos dietarios</a></li>
+                            <li><a class="dropdown-item fw-light" href="#">Suplemento Nutricional</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
         </Fragment>
     )
 }
