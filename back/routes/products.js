68b3a68329bf2516e80c8b9f4f2f3ee973fd183a
Lizeth Vega <vegalizeth4@gmail.com>
2022-11-02 21:41:16 -0500

    pruebas postman

--- back/routes/products.js
+++ back/routes/products.js
@@ -1,13 +1,13 @@
 const express=require("express")
 const router=express.Router();
 
-const {getProducts, newProduct, getProductById, updateProduct, deleteProduct} = require("../controllers/productsController"); //Traemos la respuesta json desde el controlador
+const {getProducts, newProduct, getProductsById, updateProduct, deleteProduct} = require("../controllers/productsController"); //Traemos la respuesta json desde el controlador
 //const { isAuthenticatedUser , authorizeRoles} = require("../middleware/auth");
 
 //Probemos autenticación
-router.route('/productos').get(getProducts)  //Establecemos desde que ruta queremos ver el getProducts
+router.route('/productos').get(getProducts);  //Establecemos desde que ruta queremos ver el getProducts
 router.route('/producto/nuevo').post(newProduct);
-router.route('/producto/:id').get(getProductById); //Ruta para consultar por id
+router.route('/producto/:id').get(getProductsById); //Ruta para consultar por id
 router.route('/producto/:id').put(updateProduct);//Creacion de la ruta de actualizacion
 router.route('/producto/:id').delete(deleteProduct); //Creacion de la ruta de eliminacion por id
  
