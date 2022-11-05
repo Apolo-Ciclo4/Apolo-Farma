8719aad4c843830be85ad2436c8e8701096f40f2
Lizeth Vega <vegalizeth4@gmail.com>
2022-11-04 21:50:07 -0500

    Back User

--- back/routes/products.js
+++ back/routes/products.js
@@ -1,8 +1,8 @@
 const express=require("express")
 const router=express.Router();
 
-const {getProducts, newProduct, getProductsById, updateProduct, deleteProduct} = require("../controllers/productsController"); //Traemos la respuesta json desde el controlador
-//const { isAuthenticatedUser , authorizeRoles} = require("../middleware/auth");
+const {getProducts, newProduct, getProductsById, updateProduct, deleteProduct, getProductsStock} = require("../controllers/productsController"); //Traemos la respuesta json desde el controlador
+const { isAuthenticatedUser , authorizeRoles} = require("../middleware/users");
 
 //Probemos autenticación
 router.route('/productos').get(getProducts);  //Establecemos desde que ruta queremos ver el getProducts
@@ -10,6 +10,7 @@ router.route('/producto/nuevo').post(newProduct);
 router.route('/producto/:id').get(getProductsById); //Ruta para consultar por id
 router.route('/producto/:id').put(updateProduct);//Creacion de la ruta de actualizacion
 router.route('/producto/:id').delete(deleteProduct); //Creacion de la ruta de eliminacion por id
+router.route('/admon/inventario').get(isAuthenticatedUser, getProductsStock)
  
 
 module.exports=router;
