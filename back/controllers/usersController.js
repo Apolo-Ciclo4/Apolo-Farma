cac0e5fa4445e89a876819f25be36ce16d70ee2d
Yuely Rios <yuelyrios@gmail.com>
2022-11-12 12:28:23 -0500

    Revision

--- back/controllers/usersController.js
+++ back/controllers/usersController.js
@@ -8,13 +8,14 @@ const crypto = require("crypto")
 //Regitrar un nuevo usuario /api/users/new
 //Ruta: localhost:4000/api/users/new
 exports.registroUsuario = catchAsyncErrors(async (req, res, next) => {
+    //console.log(req.body);
     const { nombre, email, password, role, celular, departamento, ciudad, direccion } = req.body;
 
     const user = await User.create({
         nombre,
         email,
         password,
-        avatar: {
+        photoPerfil: {
             public_id: "ANd9GcQKZwmqodcPdQUDRt6E5cPERZDWaqy6ITohlQ&usqp",
             url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKZwmqodcPdQUDRt6E5cPERZDWaqy6ITohlQ&usqp=CAU"
         },
